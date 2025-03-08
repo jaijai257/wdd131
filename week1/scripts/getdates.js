19552024d42f12927f8f69d4fb08156d13681c3f
jaijai257 <jeffreyohaegbulem22@gmail.com>
2025-03-08 23:41:50 +0100

    finished week 1

---
+++ week1/scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById('currentyear').textContent = currentYear;
+
+document.getElementById('lastmodified').textContent = `Last modified: ${document.lastModified}`;
